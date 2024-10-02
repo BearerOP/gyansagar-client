bd0684382c7b1ecf60cac29ac713fa472901c06c
BearerOP <ankity1892003@gmail.com>
2024-10-03 02:29:08 +0530

    modified ui of rootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Footer from '../components/footer';
 import Header from '../components/header';
 import { Outlet } from 'react-router-dom';
@@ -8,7 +7,7 @@ const RootLayout = () => {
   return (
     <div className='bg-white dark:bg-black'>
       <Header/>
-      <main className='flex flex-col justify-center items-center min-h-screen mt-20'>
+      <main className='flex flex-col min-h-screen '>
         <Outlet/>
       </main>
       <Footer/>
